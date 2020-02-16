dab19c966f52702189ec942b4d38cdaec2a67cdd
Sandesh Sonawane <56785759+sandesh421@users.noreply.github.com>
2020-02-16 18:27:48 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,20 +1,19 @@
 variable "aws_vpc" {
     description = "VPC"
-    default = "vpc-6d8f7507"
+    default = "vpc-2b7fe953"
 }
 
 variable "aws_subnet" {
     description = "aws subnet"
-    default = "subnet-81b9a0fc"
+    default = "subnet-90ff1ce8"
 }
 
 variable "aws_subnet2" {
     description = "aws subnet2"
-    default = "subnet-81b9a0fc"
+    default = "subnet-90ff1ce8"
 }
 
 variable "aws_key" {
     description = "aws key"
-    default = "DevOpsKey"
+    default = "my_oregon_key"
 }
-
