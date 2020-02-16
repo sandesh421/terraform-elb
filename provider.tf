a66a15d6354214a443b786bf64661567d1efce81
Sandesh Sonawane <56785759+sandesh421@users.noreply.github.com>
2020-02-16 18:22:14 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = "eu-central-1"
-  access_key = "***********************"
-  secret_key = "***********************"
+  region     = "us-west-2"
+  access_key = "AKIAZZX6BL4JXG6JFJDJ"
+  secret_key = "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"
 }
